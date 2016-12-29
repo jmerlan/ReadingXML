2be1ddbebef23e0a95121bc1330f70cd8d082558
J. Merlan <jr.merlan@gmail.com>
2016-12-29 06:15:54 -0800

    Fixed bug where XML file is continuously appended to dataGridView1.

--- ReadingXML/Form1.Designer.cs
+++ ReadingXML/Form1.Designer.cs
@@ -30,10 +30,13 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.ReadXmlButton = new System.Windows.Forms.Button();
-            this.AuthorsDataSet = new System.Data.DataSet();
+            this.XMLDataSet = new System.Data.DataSet();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.xmlFileBrowser = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XMLDataSet)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,44 +49,77 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(763, 432);
+            this.dataGridView1.Size = new System.Drawing.Size(988, 519);
             this.dataGridView1.TabIndex = 0;
             // 
             // ReadXmlButton
             // 
-            this.ReadXmlButton.Location = new System.Drawing.Point(676, 6);
+            this.ReadXmlButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReadXmlButton.Location = new System.Drawing.Point(910, 5);
             this.ReadXmlButton.Name = "ReadXmlButton";
             this.ReadXmlButton.Size = new System.Drawing.Size(75, 23);
             this.ReadXmlButton.TabIndex = 2;
-            this.ReadXmlButton.Text = "Read XML";
+            this.ReadXmlButton.Text = "Reload";
             this.ReadXmlButton.UseVisualStyleBackColor = true;
             this.ReadXmlButton.Click += new System.EventHandler(this.ReadXmlButton_Click);
             // 
-            // AuthorsDataSet
+            // XMLDataSet
             // 
-            this.AuthorsDataSet.DataSetName = "AuthorsDataSet";
+            this.XMLDataSet.DataSetName = "AuthorsDataSet";
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.filePathTextBox);
+            this.panel1.Controls.Add(this.xmlFileBrowser);
             this.panel1.Controls.Add(this.ReadXmlButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 432);
+            this.panel1.Location = new System.Drawing.Point(0, 519);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(763, 35);
+            this.panel1.Size = new System.Drawing.Size(988, 35);
             this.panel1.TabIndex = 4;
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.filePathTextBox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.filePathTextBox.Location = new System.Drawing.Point(12, 8);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(771, 20);
+            this.filePathTextBox.TabIndex = 3;
+            // 
+            // xmlFileBrowser
+            // 
+            this.xmlFileBrowser.AllowDrop = true;
+            this.xmlFileBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.xmlFileBrowser.Location = new System.Drawing.Point(789, 5);
+            this.xmlFileBrowser.Name = "xmlFileBrowser";
+            this.xmlFileBrowser.Size = new System.Drawing.Size(115, 23);
+            this.xmlFileBrowser.TabIndex = 1;
+            this.xmlFileBrowser.Text = "Load Clash Report";
+            this.xmlFileBrowser.UseVisualStyleBackColor = true;
+            this.xmlFileBrowser.Click += new System.EventHandler(this.xmlFileBrowser_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xml";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XML Files|*.xml";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 467);
+            this.ClientSize = new System.Drawing.Size(988, 554);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XMLDataSet)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -92,8 +128,11 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button ReadXmlButton;
-        private System.Data.DataSet AuthorsDataSet;
+        private System.Data.DataSet XMLDataSet;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button xmlFileBrowser;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox filePathTextBox;
     }
 }
 
--- ReadingXML/Form1.cs
+++ ReadingXML/Form1.cs
@@ -1,6 +1,8 @@
 ﻿// Read XML data into a dataset: https://msdn.microsoft.com/en-us/library/ekw4dh3f.aspx
+// Opening Files Using the OpenFileDialog Component: https://msdn.microsoft.com/en-us/library/aa984392(v=vs.71).aspx
 
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,21 +16,57 @@ namespace ReadingXML
 {
     public partial class Form1 : Form
     {
+
+        // File support
+        string fileNameWithPath;
+        string fileDirectory;
+        string fileName;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Browse for a file
+        private void xmlFileBrowser_Click(object sender, EventArgs e)
+        {
+            // Clear data
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Store filename and path in variables
+                fileNameWithPath = openFileDialog1.FileName;
+                fileDirectory = Path.GetDirectoryName(openFileDialog1.FileName);
+                fileName = Path.GetFileName(openFileDialog1.FileName);
+                xmlFileBrowser.Text = fileName;
+
+                // Displays the current fileNameWithPath in textbox
+                filePathTextBox.Text = fileNameWithPath;
+
+                // Create new DataSet object and print to dataGridView
+                DataSet xmlData = new DataSet();
+                xmlData.ReadXml(fileNameWithPath);
+                dataGridView1.DataSource = xmlData;
+                dataGridView1.DataMember = "clashresult";
+
+            }
+        }
+
         // Read XML file on button click
         private void ReadXmlButton_Click(object sender, EventArgs e)
         {
-            // Path to XML file
-            string filePath = @"C:\Users\jrmer\Amazon Drive\Dev Lab\ReadingXML\ReadingXMLTestContent\Mech x Elec.xml";
+            //// Reads XML file at filePath
+            //XMLDataSet.ReadXml(fileNameWithPath);
 
-            AuthorsDataSet.ReadXml(filePath);
+            //// Display XML file
+            //dataGridView1.DataSource = XMLDataSet;
+            //dataGridView1.DataMember = "clashresult";
 
-            dataGridView1.DataSource = AuthorsDataSet;
-            dataGridView1.DataMember = "clashresult";
+            //// Display current file in textbox
+            //filePathTextBox.Text = fileNameWithPath;
         }
+
     }
 }
